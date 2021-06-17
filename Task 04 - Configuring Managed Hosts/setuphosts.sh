5fb8c70a70398388ef11f8c47a90d01b0740e57a
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2021-06-17 15:18:48 -0400

    Update setuphosts.sh

--- Task 04 - Configuring Managed Hosts/setuphosts.sh
+++ Task 04 - Configuring Managed Hosts/setuphosts.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-ansible all -u root -m user -a "name=ansible password=password append=yes groups=wheel state=present" -e ansible_password=password
+ansible all -u root -m user -a "name=ansible append=yes groups=wheel state=present" -e ansible_password=password
+ansible all -u root -m shell -a "echo password | passwd ansible --stdin" -e ansible_password=password
+ansible all -u root -m shell -a 'echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible' -e ansible_password=password
+
+ansible all -m authorized_key -a "user=ansible state=present key='{{ lookup('file','/home/ansible/.ssh/id_rsa.pub')}}'" -e ansible_password=password
+
+ansible all -m ping
